1b9bfe1fa4bc0d6ff109f07a812026ff6ff74551
itzguled <itzguled55337@gmail.com>
2022-01-11 18:50:26 -0600

    task

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,24 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 var createTaskHandler = function(event) { 
   event.preventDefault(); 
+  
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    // creat list item
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
+    //creat div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    //give is a class name
+    taskInfoEl.className = "task-info";
+    //add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "<span>";
 
-  var listItemEl = document.createElement("li"); 
-  listItemEl.className = "task-item"; 
-  listItemEl.textContent = "This is a new task."; 
-  tasksToDoEl.appendChild(listItemEl); 
+    listItemEl.appendChild(taskInfoEl);
+
+    //add entire list item to list
+    tasksToDoEl.appendChild(listItemEl);
   }; 
 
   formEl.addEventListener("submit", createTaskHandler);
